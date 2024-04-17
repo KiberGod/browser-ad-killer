5808700a4bd11fbfeea57f1c2ac3a2312a51126a
KiberGod <stupakov.kirill13@gmail.com>
2024-04-18 01:33:53 +0300

    Update for ytp-skip-ad-button

--- content.js
+++ content.js
@@ -19,20 +19,23 @@ function hideRightAdBanner(id) {
 // Прослуховувач реклами на YouTube
 function adDetection() {
   hideStaticAd();
-  if(document.querySelector('.ytp-ad-player-overlay')) {  // Зараз показують рекламу ?
+  if(document.querySelector('.ytp-ad-player-overlay') || document.querySelector('.ytp-ad-player-overlay-layout')) {  // Зараз показують рекламу ?
     //console.log("Зараз показують рекламу")
     const skipButton = document.querySelector("button.ytp-ad-skip-button");
+    const skipButton2 = document.querySelector("button.ytp-skip-ad-button");
     const skipButtonModern = document.querySelector("button.ytp-ad-skip-button-modern")
 
     if (skipButtonModern) {
       skipButtonModern.click();
     } else if (skipButton) {
       skipButton.click();
+    } else if (skipButton2) {
+      skipButton2.click();
     } else {
       var video = document.querySelectorAll("video.video-stream.html5-main-video")[0];
       video.currentTime = video.duration;
     }
-  }
+  } 
   hideRightAdBanner("player-ads");
   hideRightAdBanner("panels");
 }
